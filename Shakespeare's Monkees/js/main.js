62b2b1d42cbb31ab3cde4fb5076c05180bf85a0f
Andrew <sacarter@live.com>
2018-02-11 15:05:13 -0500

    All Inputs Working
    ToDo: Begin Algroithm

---
+++ Shakespeare's Monkees/js/main.js
@@ -0,0 +1,6 @@
+
+  // Global Variables
+  var targetPhrase = "";      // The Target Phrase to generate
+  var populationSize = 0;     // The Population Size
+  var mutationRate = 0;       // Mutation Rate
+  var currentGeneration = 0; // Current Generation Counter
