cf73e2f379560e6fc061639bb875985de222181f
Andrew <sacarter@live.com>
2018-04-13 22:12:41 -0400

    Restart

--- Shakespeare's Monkees/js/main.js
+++
@@ -1,6 +0,0 @@
-
-  // Global Variables
-  var targetPhrase = "";      // The Target Phrase to generate
-  var populationSize = 0;     // The Population Size
-  var mutationRate = 0;       // Mutation Rate
-  var currentGeneration = 0; // Current Generation Counter
